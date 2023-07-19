4eb2b42bb90210954ac5d52fc925650f26621d6b
✧Victor✧ <68920741+vkrenz@users.noreply.github.com>
2023-07-19 16:14:01 -0400

    Update pin.js

--- backend/schemas/pin.js
+++ backend/schemas/pin.js
@@ -32,9 +32,34 @@ export default {
             }
         },
         {
-            name: 'title',
-            title: 'Title',
+            name: 'userId',
+            title: 'UserID',
             type: 'string'
+        },
+        {
+            name: 'postedBy',
+            title: 'PostedBy',
+            type: 'postedBy'
+        },
+        {
+            name: 'save',
+            title: 'Save',
+            type: 'array',
+            of: [
+                {
+                    type: 'save'
+                }
+            ]
+        },
+        {
+            name: 'comments',
+            title: 'Comments',
+            type: 'array',
+            of: [
+                {
+                    type: 'comment'
+                }
+            ]
         },
     ]
 }
