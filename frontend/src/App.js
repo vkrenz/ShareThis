eee8904ff4d8be2d96bdffb99f0fd73cca9ea47e
✧Victor✧ <68920741+vkrenz@users.noreply.github.com>
2023-07-21 16:53:46 -0400

    Added React-Router-Dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import {
+  Routes,
+  Route,
+  useNavigation
+} from 'react-router-dom'
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world.
-    </h1>
+    <Routes>
+      <Route path="login" element={<Login />} />
+      <Route path="/*" element={<Home />} />
+    </Routes>
   )
 }
 
