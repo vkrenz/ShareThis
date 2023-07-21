955263211fd4f9f6f8abcfcf5b0ba76bf919a53e
✧Victor✧ <68920741+vkrenz@users.noreply.github.com>
2023-07-21 17:24:12 -0400

    added google login button

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -21,8 +21,24 @@ const Login = () => {
           />
 
           <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
-            <div className="p-5">
+            <div className="p-5 border rounded-lg">
               <img src={logo} width="130px" alt="logo" />
+              <hr className="my-5"/>
+              <div className="shadow-2xl">
+                <GoogleLogin
+                  clientId=''
+                  render={(renderProps) => (
+                    <button
+                      type="button"
+                      className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
+                      onClick={renderProps.onClick}
+                      disabled={renderProps.disabled}
+                    >
+                      <FcGoogle className="mr-4" /> Sign in with Google
+                    </button>
+                  )}
+                />
+              </div>
             </div>
           </div>
         </div>
